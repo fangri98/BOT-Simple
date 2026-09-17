bdefe133a8e0db7051e4d0d55822404589de483f
ridho <222992681+fangri98@users.noreply.github.com>
2026-09-17 13:18:04 +0700

    up

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import calculate as cal
 import time_date as td
 import random as rd
-print("BOT FANG")
 
+print("BOT FANG")
 
 
 while True:
@@ -32,13 +32,13 @@ while True:
             except ValueError:
                 print("Tolong masukkan angka yang valid.")
                 continue
-            
+
             if choice == 5:
                 print("Terima kasih telah menggunakan kalkulator. Sampai jumpa!")
                 break
             num1 = float(input("Masukkan angka pertama: "))
             num2 = float(input("Masukkan angka kedua: "))
-            
+
             if choice == 1:
                 result = cal.tambah(num1, num2)
                 print(f"Hasil: {num1} + {num2} = {result}")
@@ -56,8 +56,8 @@ while True:
                 print(f"Hasil: {num1} / {num2} = {result}")
                 break
             else:
-                print("pilihan tidak valid. silahkan coba lagi.")    
-                
+                print("pilihan tidak valid. silahkan coba lagi.")
+
     elif user_input == 2:
         print("Selamat datang di Kalender!")
         while True:
@@ -87,13 +87,13 @@ while True:
                 print("Pilihan tidak valid. Silahkan coba lagi.")
     elif user_input == 3:
         print("Selamat datang di permainan Tebak Angka!")
-        
+
         print("\nSaya telah memilih sebuah angka antara 1 dan 50.")
         print("Coba tebak angka tersebut!")
-            
+
         secret_number = rd.randint(1, 50)
         attempts = 0
-            
+
         while True:
             try:
                 guess = int(input("Masukkan tebakan anda(jika ingin keluar ketik 0): "))
@@ -104,27 +104,31 @@ while True:
             if guess == 0:
                 print("Terima kasih telah bermain. Sampai jumpa!")
                 break
-            
+
             try:
                 guess = int(guess)
             except ValueError:
                 print("Tolong masukkan angka yang valid.")
                 continue
-        
+
             attempts += 1
-            
+
             if attempts >= 5:
-                print(f"Maaf, anda telah mencapai batas percobaan. Angka yang benar adalah {secret_number}.")
+                print(
+                    f"Maaf, anda telah mencapai batas percobaan. Angka yang benar adalah {secret_number}."
+                )
                 break
             if guess == secret_number:
-                print(f"Selamat! Anda menebak angka yang benar {secret_number} dalam {attempts} percobaan.")
+                print(
+                    f"Selamat! Anda menebak angka yang benar {secret_number} dalam {attempts} percobaan."
+                )
                 break
             elif guess < secret_number:
                 print("Tebakan anda terlalu rendah. Coba lagi!")
                 attempts += 1
             else:
                 print("Tebakan anda terlalu tinggi. Coba lagi!")
-                
+
     elif user_input == 4:
         print("Terima kasih telah menggunakan BOT FANG. Sampai jumpa!")
         break
